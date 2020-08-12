046cf9f49f62944a1e3a9fb8aac00a488c9b23ee
asapapi <46496796+asapapi@users.noreply.github.com>
2020-08-11 23:12:21 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+// You are to debug this program by correcting any syntax errors so 
+// the program will compile & execute 
+// Additionally, you are to answer the questions in the accompanying 
+// Debug Questions.docx Word document (it is in the same folder as
+// the Debug Exercise.sln file
+
+namespace Debug_Exercise
+{
+    class Program
+    {
+        static Random gener = new Random(Support.GetSeed());
+
+        const int MIN = Support.MIN;
+        const int MAX = Support.MAX;
+
+        static string [] names = { "ybraD", "ecnerwaL", "haraS", "refinneJ", "yevaD" };
+
+        static void Main(string[] args)
+        {
+            int age = Support.GetAge(gener.Next(MIN, MAX));
+            string name = Support.GetName(names[gener.Next(names.Length)]);
+            //instead of ag its age according to the constructor
+            DisplayUserInfo(name, age);
+
+            SayABCs();
+
+            Console.ReadKey();
+        }
+
+        public static void DisplayUserInfo(string who, int age)
+        {
+            Console.Write("Please provide a warm welcome to my next guest, ");
+            Console.WriteLine(who + ", on the occasion of turning " + age + " years old!");
+        }
+
+        public static void SayABCs()
+        {
+            for(int ndx = 97; ndx <= 122; ndx++)
+            {
+                if ((ndx + 3) % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+                if (ndx == 97)
+                    Console.Write("\n" + (char)ndx + " ");
+                else
+                    Console.Write((char)ndx + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Support.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debug_Exercise {
+    class Support {
+        public const int MAX = 199;
+        public const int MIN = 110;
+        static int seed = new Random().Next();
+
+        public static int GetSeed() {
+            return seed;
+        }
+
+        public static string GetName(string name) {
+            name = ReverseString(name);
+            return name;
+        }
+
+        public static int GetAge(int age) {
+            return age - 100;
+        }
+
+        private static string ReverseString(string source) {
+            string result = "";
+            for (int ndx = source.Length - 1; ndx >= 0; ndx--) {
+                result += source[ndx];
+            }
+            return result;
+        }
+        static int Test3(String input) {
+            return Int32.Parse(input);
+        }
+
+        static sbyte Test4(String input) {
+            return SByte.Parse(input);
+        }
+        static double Test5(String input) {
+            return Double.Parse(input);
+        }
+        static ushort Test6(String input) {
+            return UInt16.Parse(input);
+
+        }
+        static float Test7(String input) {
+            return float.Parse(input);
+        }
+        static uint Test8(String input) {
+            return UInt32.Parse(input);
+        }
+        static short Test9(String input) {
+            return Int16.Parse(input);
+        }
+        static ulong Test10(String input) {
+            return UInt64.Parse(input);
+        }
+    }
+}
+
